78ad18a4f7ed0ba99ff0d40949591967e3d3863f
Arman Ahmed Shawon <shawon5ice@gmail.com>
2025-01-28 08:58:52 +0600

    -----

--- lib/features/accounting/presentation/accounting_screen.dart
+++ lib/features/accounting/presentation/accounting_screen.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
-class AccountingScreen extends StatelessWidget {
+class AccountingScreen extends StatefulWidget {
   const AccountingScreen({super.key});
+
+  @override
+  State<AccountingScreen> createState() => _AccountingScreenState();
+}
+
+class _AccountingScreenState extends State<AccountingScreen> {
+  List<String> accounting = [
+    "Daily Statement",
+    "Expense Voucher",
+    "Due Collection",
+    "Due Payment",
+    "Money Transfer",
+    "Money Adjustment",
+    "Ledger",
+    "Trial Balance",
+    "Profit or Loss",
+    "Balance Sheet",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: AppBar(title: Text('Accounting'),centerTitle: true,),
+        title: AppBar(
+          title: Text('Accounting'),
+          centerTitle: true,
+        ),
       ),
-      body: Column(
-
-      ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        childAspectRatio: 166/53,
+        children: <Widget>[
+          ...accounting.map((e) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(color: Color(0xffFF9000), width: .5)
+              ),
+              child: Center(child: Text(e))),)
+        ],
+      )
     );
   }
 }
